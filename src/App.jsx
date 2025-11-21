0aa6c021882b880e2fa27d3c9190bed0ca4b283b
Codewithgoostyhuman <zeeshantahiralvi123@gmail.com>
2025-11-21 22:42:15 +0500

    announcemnt section - 1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Announcment from './components/announcmentSection/Announcment'
+
+function App() {
+  
+
+  return (
+    <div>
+     {/*Announcement section*/}
+      <Announcment />
+     
+    </div>
+  )
+}
+
+export default App
